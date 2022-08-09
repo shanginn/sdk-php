e3ea40c87bb34aecf927b3d21e5560369325293f
Shangin Nikolai <shanginn@gmail.com>
2022-08-09 11:28:51 +0800

    some casting

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,8 +6,8 @@ use Temporal\Testing\Environment;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-var_dump(getenv('RUN_TEMPORAL_TEST_SERVER'));
-if (getenv('RUN_TEMPORAL_TEST_SERVER') !== false) {
+var_dump(boolval(getenv('RUN_TEMPORAL_TEST_SERVER')));
+if (boolval(getenv('RUN_TEMPORAL_TEST_SERVER')) !== false) {
     $environment = Environment::create();
     $environment->start('./rr serve -c .rr.silent.yaml -w tests');
     register_shutdown_function(fn() => $environment->stop());
